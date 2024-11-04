8307ba75b5682b461bad683b72f144e21a5261a1
Amiirez <famirco@gmail.com>
2024-11-04 06:37:12 +0000

    fix next.config.js conflicts

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,18 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-<<<<<<< HEAD
-  distDir: '.next',
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://amirez.info' : '',
-  experimental: {
-    outputStandalone: true
-  }
-=======
-  assetPrefix: '',
-  images: {
-    domains: ['amirez.info'],
-  },
->>>>>>> 9d95b706adc71cad6cc9752fced3cacf17248525
+    output: 'standalone',
+    experimental: {
+        serverActions: true,
+    }
 }
 
 module.exports = nextConfig
