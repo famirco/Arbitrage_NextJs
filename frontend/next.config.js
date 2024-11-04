d18644340c276a36f734d5f5739c4def8e8cb3cf
Amir Dezyanian <30830017+famirco@users.noreply.github.com>
2024-11-04 17:31:39 +0330

    init

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
-    experimental: {
-      outputStandalone: true
-    },
-    // اضافه کردن basePath و assetPrefix
-    basePath: '',
-    assetPrefix: ''
+  output: 'standalone',
+  experimental: {
+    outputStandalone: true
+  },
+  assetPrefix: 'https://amirez.info',
+  images: {
+    domains: ['amirez.info'],
   }
-  
-  module.exports = nextConfig
+}
+
+module.exports = nextConfig
