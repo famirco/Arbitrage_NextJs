6cb05a8a96970e7856aa73280ce9c30ce7a94d1b
Amir Dezyanian <30830017+famirco@users.noreply.github.com>
2024-11-05 13:26:05 +0330

    init

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  assetPrefix: 'https://amirez.info'
+  output: 'standalone'
 }
 
 module.exports = nextConfig
