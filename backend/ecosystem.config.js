514ca0768954b340984fb1e3833649b77f885171
Amir Dezyanian <30830017+famirco@users.noreply.github.com>
2024-11-06 22:14:16 +0330

    init

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,12 +1,32 @@
 module.exports = {
-    apps: [{
+    apps: [
+      {
         name: 'arbitrage-backend',
         script: 'server.ts',
         interpreter: 'node',
         interpreter_args: '-r ts-node/register',
+        instances: 1,
+        exec_mode: 'fork',
+        watch: false,
+        max_memory_restart: '500M',
         env: {
-            NODE_ENV: 'production',
-            PORT: 3001
+          NODE_ENV: 'production',
+          PORT: 3001
         }
-    }]
-}
+      },
+      {
+        name: 'arbitrage-frontend',
+        cwd: '../frontend',
+        script: 'npm',
+        args: 'start',
+        instances: 1,
+        exec_mode: 'fork',
+        watch: false,
+        max_memory_restart: '500M',
+        env: {
+          NODE_ENV: 'production',
+          PORT: 3000
+        }
+      }
+    ]
+  };
